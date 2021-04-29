1a0f9ffa929088ca16c548d2356d2fe3548a9d15
Sur1379 <Suren99999@mail.ru>
2021-04-29 21:11:29 +0400

    day2

---
+++ sample12.java
@@ -0,0 +1,12 @@
+package com.company.TaskDay2;
+
+import java.util.Scanner;
+
+public class sample12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+         int num = scanner.nextInt();
+         boolean a = num%2 == 0;
+        System.out.println(a);
+    }
+}
---
+++ sample7.java
@@ -0,0 +1,13 @@
+package com.company.TaskDay2;
+
+public class sample7 {
+    public static void main(String[] args) {
+        int n = 10;
+        int Sn = 3 * n * n - 12 * n;
+        System.out.println(Sn);
+        n = 15;
+        Sn = 3 * n * n - 12 * n;
+        System.out.println(Sn);
+
+    }
+}
